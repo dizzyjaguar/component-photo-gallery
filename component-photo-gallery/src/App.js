d29b545ea30adc1043142fe17cb1352d5f2a7a36
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-12 11:16:08 -0800

    filtering working

--- component-photo-gallery/src/App.js
+++ component-photo-gallery/src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Header from './Header.js'
 import ImageList from './ImageList.js'
 import images from './data/images.js'
-
-
 import './App.css';
 
-//NEXT I need to work on the the ImageItem.js and create a component that will live inside the ImageList component, I think that will be done in App.js
 
 function App() {
   return (
     <div>
 
       <Header> </Header>
-    
+      
+      {/* creating key value pairs/properties */}
       <ImageList imageData={images} />
     
     </div>
--- component-photo-gallery/src/ImageItem.js
+++ component-photo-gallery/src/ImageItem.js
@@ -6,7 +6,6 @@ export default class ImageItem extends Component {
             <li className='image-item'>
                 <h3>{this.props._image.title}</h3>
                 <img src={this.props._image.url}></img>
-
             </li>
                 
                
